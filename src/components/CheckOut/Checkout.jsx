21fad354acbf3e0a0772a191ba8e5f02e7d5d420
Patrick Shepherd <patrickshepherd218@gmail.com>
2022-10-18 11:37:47 +0100

    deleted dupe file

--- src/components/CheckOut/Checkout.jsx
+++
@@ -1,5 +0,0 @@
-const delAlldrinks = async () => {
-    const data = await getDocs(collection(db, `${userName}/items/drinks`));
-    setdrinks(data.docs.map((docu) => (deleteDoc(doc(db, `${userName}/items/drinks`, docu.id)))));
-
-};
