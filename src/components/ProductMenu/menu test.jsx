33339892d71a61c3467ba8f27a8aa5f4bb1cff3a
Patrick Shepherd <patrickshepherd218@gmail.com>
2022-10-19 09:50:09 +0100

    fixed menu

---
+++ src/components/ProductMenu/menu test.jsx
@@ -0,0 +1,58 @@
+
+<Container fluid={true}>
+  <Row>
+    {checkOut ? (<div><div className="col-4">
+              <CheckoutPage
+                key="CheckoutPage"
+                userOrTable={userOrTable}
+                tableName={tableName}
+                basketTotal={basketTotal}
+                setBasketTotal={setBasketTotal}
+              />{' '}
+            </div>
+            <div className="col-4"></div></div>) : null}
+    {products ? (<div className="col-8 menu">
+      <Container>
+        <Row>
+          {items.map((item) => {
+            return (
+              <SingleItemButton
+                tableName={tableName}
+                name={item.name}
+                price={item.price}
+                id={item.id}
+                setNewCounter={setNewCounter}
+                counter={counter}
+                userOrTable={userOrTable}
+              />
+            );
+          })}
+        </Row>
+      </Container>
+    </div>) : null}
+    {!products ? (
+      <Tables
+        key="table"
+        setTableName={setTableName}
+        setProducts={setProducts}
+        setNewCounter={setNewCounter}
+        counter={counter}
+        setUserOrTable={setUserOrTable}
+      />
+    ) : null}
+    <div className="order-box col-4">
+      <Container>
+        <MenuBasket
+          setNewCounter={setNewCounter}
+          key="MenuBasket"
+          counter={counter}
+          tableName={tableName}
+          userOrTable={userOrTable}
+          setCheckout={setCheckout}
+          basketTotal={basketTotal}
+          setBasketTotal={setBasketTotal}
+        />
+      </Container>
+    </div>
+  </Row>
+</Container >
